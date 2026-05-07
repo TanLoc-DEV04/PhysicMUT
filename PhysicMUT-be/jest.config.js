85be5b91c8be5b0f48f4a9ce6b02ce23f33683a1
TanLoc-DEV04 <nguyenquocloc2004@email.com>
2026-05-07 16:47:39 +0700

    Enable Jest coverage collection and SonarCloud integration
    - Configure Jest to collect coverage in lcov format
    - Enable SonarCloud to read coverage reports from coverage/lcov.info
    - This allows SonarCloud badge to display coverage percentage

--- PhysicMUT-be/jest.config.js
+++ PhysicMUT-be/jest.config.js
@@ -8,4 +8,8 @@ module.exports = {
   transform: {
     ...tsJestTransformCfg,
   },
+  // --- Thêm phần cấu hình Coverage ở đây ---
+  collectCoverage: true,                 // Bật tính năng thu thập coverage
+  coverageDirectory: "coverage",         // Thư mục chứa file báo cáo xuất ra
+  coverageReporters: ["lcov", "text"],   // Định dạng xuất ra (lcov cho SonarCloud, text để xem trên terminal)
 };
