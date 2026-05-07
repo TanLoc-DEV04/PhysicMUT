810f7363ce65f05e0cbf701f28e3ffcc47dc56a6
TanLoc-DEV04 <nguyenquocloc2004@email.com>
2026-05-07 17:31:22 +0700

    Disable coverage collection in Jest

--- PhysicMUT-be/jest.config.js
+++ PhysicMUT-be/jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
     ...tsJestTransformCfg,
   },
   // --- Thêm phần cấu hình Coverage ở đây ---
-  collectCoverage: true,                 // Bật tính năng thu thập coverage
-  coverageDirectory: "coverage",         // Thư mục chứa file báo cáo xuất ra
-  coverageReporters: ["lcov", "text"],   // Định dạng xuất ra (lcov cho SonarCloud, text để xem trên terminal)
+  // collectCoverage: true,                 // Bật tính năng thu thập coverage
+  // coverageDirectory: "coverage",         // Thư mục chứa file báo cáo xuất ra
+  // coverageReporters: ["lcov", "text"],   // Định dạng xuất ra (lcov cho SonarCloud, text để xem trên terminal)
 };
