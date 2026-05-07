f416e97452d00086a81e611978c8ce1c5e41384b
TanLoc-DEV04 <nguyenquocloc2004@email.com>
2026-05-07 19:11:08 +0700

    Fix: remove trailing slash in CORS origin

--- PhysicMUT-be/src/middlewares/securityMiddleware.ts
+++ PhysicMUT-be/src/middlewares/securityMiddleware.ts
@@ -93,7 +93,7 @@ export const getAllowedOrigins = (): string[] => {
     "http://localhost:4173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "https://physic-mut.vercel.app/",
+    "https://physic-mut.vercel.app",
   ];
 
   const productionOrigin = process.env.FRONTEND_URL;
